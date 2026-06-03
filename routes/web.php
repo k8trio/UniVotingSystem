0d959d202b3f6f9d647d25d9ff02310ab4561c4e
nabi-hermione <katediane023@gmail.com>
2026-06-03 17:57:56 +0800

    updated layout and web.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/ballot', function () {
+    return view('ballot');
+});
+
+Route::get('/review', function () {
+    return view('review');
+});
+
+Route::get('/voted', function () {
+    return view('voted');
+});
+
+Route::get('/transparency', function () {
+    return view('transparency');
+});
+
+Route::get('/admin', function () {
+    return view('admin');
 });
