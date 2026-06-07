0a91f8d357c2fbd598d34fab33d9be9df5f7f15d
Kate Diane Inoue <katediane023@gmail.com>
2026-06-07 20:22:05 +0800

    Update TransparencyController.php

--- app/Http/Controllers/TransparencyController.php
+++ app/Http/Controllers/TransparencyController.php
@@ -17,7 +17,7 @@ class TransparencyController extends Controller
                     ->orderBy('first_name');
             }])
             ->orderBy('display_order')
-            ->get();
+            ->paginate(5);
 
         $voteCounts = Vote::selectRaw('candidate_id, COUNT(*) as total_votes')
             ->whereNotNull('candidate_id')
